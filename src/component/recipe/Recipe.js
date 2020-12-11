a3d753ebcbb54f15d975d7749c6cbcb802b50c85
Abhishek <abhishekyadavfeb1498@gmail.com>
2020-12-11 21:12:16 +0530

    css added

--- src/component/recipe/Recipe.js
+++ src/component/recipe/Recipe.js
@@ -1,6 +1,19 @@
 import React from 'react';
-
+import  './Recipe.css'
 const Recipe = () => {
-  return <h1>This is your Recipe</h1>;
+  return (
+    
+    <div className='whole_container'>
+      <h1 className='heading'>Here is your Recipe</h1>
+      <div className='recipe_box'>
+        <ul>
+          <li>Have some Paneer</li>
+          
+        </ul>
+      </div>
+      <div className='thank_you_button'>Thank You</div>
+      </div>
+    
+  );
 };
 export default Recipe;
