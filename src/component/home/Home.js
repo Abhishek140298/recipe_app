134baf5fea2e609beebe87fbd3c1b211d81a61f5
Abhishek <abhishekyadavfeb1498@gmail.com>
2020-12-18 19:43:24 +0530

    Pagination Added

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,22 +1,44 @@
-import React, { useEffect ,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Dishes from '../../component/dishes/Dishes';
 import { getRecipeData } from '../../actions/RecipeDataAction';
 import { connect } from 'react-redux';
+import ReactPaginate from 'react-paginate';
+import './Home.css';
 
 const Home = (props) => {
-  const[currentPage,setCurrrentPage]=useState(0);
-  const perPage=2;
+  const [currentPage, setCurrrentPage] = useState(0);
+
   useEffect(() => {
     props.getRecipeData();
   }, []);
+  const perPage = 2;
+  const offset = currentPage * perPage;
+  const pageCount = Math.ceil(props.recipeData .length/ perPage);
   
+  function handlePageClick({ selected: selectedPage }) {
+    console.log("page",selectedPage)
+    setCurrrentPage(selectedPage);
+  }
 
   return (
     <>
-		<h1 className='dishes_heading'>Choose Your Dishes</h1>
-      {props.recipeData.map((cval, index) => {
-        return <Dishes name={cval.dishName} image={cval.dishImage} id={cval.id} />;
+      <h1 className='dishes_heading'>Choose Your Dishes</h1>
+      {props.recipeData.slice(offset, offset + perPage).map((cval, index) => {
+        return (
+          <Dishes name={cval.dishName} image={cval.dishImage} id={cval.id} />
+        );
       })}
+      <ReactPaginate
+        previousLabel={'← Previous'}
+        nextLabel={'Next →'}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination'}
+        previousLinkClassName={'pagination__link'}
+        nextLinkClassName={'pagination__link'}
+        disabledClassName={'pagination__link--disabled'}
+        activeClassName={'pagination__link--active'}
+      />
     </>
   );
 };
